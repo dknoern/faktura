fd8c18f3abbc2e8b1d18cf0ee5c5d5d801db11b2
David Knoernschild <david@seattleweb.com>
2025-03-19 11:52:36 -0700

    change new customer icon color to black

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -2,6 +2,7 @@ import { CustomersTable } from "@/components/customers/table";
 import { SkeletonTable } from "@/components/skeletons";
 import { Suspense } from "react";
 import { Plus } from "lucide-react";
+import { buttonVariants } from "@/components/ui/button"
 import Link from "next/link";
 
 export default async function Page() {
@@ -11,7 +12,7 @@ export default async function Page() {
         <h2 className='text-2xl font-bold tracking-tight'>Customers</h2>
         <Link
           href="/dashboard/customers/new"
-          className="inline-flex items-center gap-2 px-4 py-2 text-sm font-medium text-white bg-blue-600 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+          className={buttonVariants({ variant: "default" })}
         >
           <Plus className="w-4 h-4" />
           New Customer
